d68c3ecc56bdd0bb540de7acf95030e7cc23b434
tuutti <tuutti@iki.fi>
2013-08-29 19:58:56 +0300

    toggle between workspace windows

--- .i3/scripts/i3-window-switcher.py
+++ .i3/scripts/i3-window-switcher.py
@@ -3,8 +3,22 @@ import i3
 import argparse
 from pprint import pprint
 from sys import exit
+import os
+import cPickle as pickle
 
 # Requirements: https://github.com/ziberna/i3-py
+def set_last_window(pickle_file, window):
+  with open(pickle_file, 'w') as handle:
+    pickle.dump(window, handle)
+
+def get_last_window(pickle_file):
+  if not os.path.exists(pickle_file):
+    return None
+
+  with open(pickle_file) as handle:
+    result = int(pickle.load(handle))
+  
+  return result
 
 def get_current_workspace():
   current = [ws for ws in i3.get_workspaces() if ws['focused']][0]
@@ -29,6 +43,8 @@ def get_window_ids(nodes, window_ids = []):
   return window_ids
 
 if __name__ == '__main__':
+  pickle_file = '/tmp/last_window.pickle'
+
   parser = argparse.ArgumentParser()
   parser.add_argument('-n')
 
@@ -37,17 +53,26 @@ if __name__ == '__main__':
   if not args.n:
     exit('No window number given')
 
-  selected = int(args.n)
-  current = get_current_workspace()
+  selected  = int(args.n)
+  workspace = get_current_workspace()
 
-  if not current:
+  if not workspace:
     exit('Failed to get current workspace')
 
-  windows    = i3.filter(num=current['num'])[0]
+  windows    = i3.filter(num=workspace['num'])[0]
   window_ids = get_window_ids(windows['nodes'])
+  # Currently focused window
+  current    = i3.filter(nodes=[], focused=True)[0]['id']
+  last       = get_last_window(pickle_file)
 
   if selected > len(window_ids):
     exit('Index out of range')
   
   if window_ids[selected]:
-    i3.focus(con_id=window_ids[selected])
+    if window_ids[selected] == current:
+      i3.focus(con_id=last)
+    else:
+      i3.focus(con_id=window_ids[selected])
+
+    set_last_window(pickle_file, current)
+  
