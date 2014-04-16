22d730b61a9f2158ef5b9990eeacb898dd44d036
Lari Rauno <tuutti@iki.fi>
2014-04-16 06:18:00 +0300

    nfs mount script

---
+++ nfs-mount.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo umount -l /media/network/plex-movies
+sudo umount -l /media/network/plex
+
+ssh deluge@seedbox -fNL 2049:localhost:2049
+sudo mount -v -t nfs -o port=2049,tcp localhost:/home/deluge/completed/btncompleted /media/network/plex
+sudo mount -v -t nfs -o port=2049,tcp localhost:/home/deluge/completed/movies /media/network/plex-movies
