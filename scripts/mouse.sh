57cbf09b2ce152fb0e1cc0e5f3e5363875f2dea5
Lari Rauno <tuutti@iki.fi>
2015-10-31 04:10:45 +0200

    adjusted mouse.sh for new mouse

--- scripts/mouse.sh
+++ scripts/mouse.sh
@@ -7,5 +7,9 @@ if [ $# -eq 0 ]
 fi
 
 MOUSE=$1
-xinput --set-prop "$MOUSE" "Device Accel Constant Deceleration" 1.7
-xinput --set-prop "$MOUSE" "Device Accel Velocity Scaling" 10
+SS_DECELERATION=1.7
+SS_SCALING=10
+ZOWIE_DECELARTION=0.85
+ZOWIE_SCALING=10
+xinput --set-prop "$MOUSE" "Device Accel Constant Deceleration" $ZOWIE_DECELARTION
+xinput --set-prop "$MOUSE" "Device Accel Velocity Scaling" $ZOWIE_SCALING
