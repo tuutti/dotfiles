7e8db65b6b3dbf7cb7ccc046068655b6871d8127
Lari Rauno <tuutti@iki.fi>
2015-12-11 18:27:05 +0200

    removed some unused shit

--- scripts/mouse.sh
+++ scripts/mouse.sh
@@ -13,4 +13,3 @@ ZOWIE_DECELARTION=0.85
 ZOWIE_SCALING=10
 xinput --set-prop "$MOUSE" "Device Accel Constant Deceleration" $ZOWIE_DECELARTION
 xinput --set-prop "$MOUSE" "Device Accel Velocity Scaling" $ZOWIE_SCALING
-xinput set-button-map $MOUSE 1 2 3 4 5 6 7 2 9 10 11 12 13 14
