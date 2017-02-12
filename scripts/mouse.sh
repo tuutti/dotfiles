83156a111b274be99c872ec95a09c8b6efa4154b
Lari Rauno <tuutti@iki.fi>
2017-02-12 17:25:51 +0200

    mouse acceleration xorg config, mouse adjustments

--- scripts/mouse.sh
+++ scripts/mouse.sh
@@ -5,23 +5,13 @@ if [ $# -eq 0 ]
     echo "No arguments supplied"
     exit 1
 fi
-
+SPEED=-0.39
 MOUSE=$1
-DEVICE=$2
 
-case "$DEVICE" in
-  "raw")
-    DECELERATION=1.9
-    SCALING=10
-    ;;
-  "ss")
-    DECELERATION=1.7
-    SCALING=10
-    ;;
-  "zowie")
-    DECELERATION=0.85
-    SCALING=10
-    ;;
-esac
-xinput --set-prop "$MOUSE" "Device Accel Constant Deceleration" $DECELERATION
-xinput --set-prop "$MOUSE" "Device Accel Velocity Scaling" $SCALING
+if [ "$2" == "track" ]; then
+  # Remap middleclick to back button
+  # @see http://wiki.mbirth.de/know-how/software/linux/remapping-mouse-buttons.html for mappings
+  xinput set-button-map "$MOUSE" 1 9 3 4 6 4 7 8
+  SPEED=-0.9
+fi
+xinput set-prop "$MOUSE" "libinput Accel Speed" $SPEED
