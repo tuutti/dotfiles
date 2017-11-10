36a87436752a1e7b09ce897ffcaf3dba0e47c0a9
Lari Rauno <tuutti@iki.fi>
2017-11-10 13:40:31 +0200

    More trackball fixes

--- scripts/mouse.sh
+++ scripts/mouse.sh
@@ -12,9 +12,10 @@ if [ "$2" == "track" ]; then
   # Remap middleclick to back button
   # @see http://wiki.mbirth.de/know-how/software/linux/remapping-mouse-buttons.html for mappings
   xinput set-button-map "$MOUSE" 1 9 3 4 6 4 7 8
-  SPEED=-0.45
   xinput --set-prop "$MOUSE" "libinput Scroll Method Enabled" 0 0 1
   xinput --set-prop "$MOUSE" "libinput Button Scrolling Button" 3
-  xinput --set-prop "$MOUSE" "Evdev Wheel Emulation Axes" 6 7 4 5
+  xinput --set-prop "$MOUSE" "libinput Accel Profile Enabled" 0 0
+  xinput set-prop "$MOUSE" "libinput Accel Speed" -0.6
+else
+  xinput set-prop "$MOUSE" "libinput Accel Speed" $SPEED
 fi
-xinput set-prop "$MOUSE" "libinput Accel Speed" $SPEED
