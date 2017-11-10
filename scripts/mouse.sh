a44a328e14ed7bbc9668dfec68fbdb8c7ac3ac96
Lari Rauno <tuutti@iki.fi>
2017-11-10 13:30:42 +0200

    Fixed trackball scroll

--- scripts/mouse.sh
+++ scripts/mouse.sh
@@ -5,13 +5,16 @@ if [ $# -eq 0 ]
     echo "No arguments supplied"
     exit 1
 fi
-SPEED=-0.39
+SPEED=-0.65
 MOUSE=$1
 
 if [ "$2" == "track" ]; then
   # Remap middleclick to back button
   # @see http://wiki.mbirth.de/know-how/software/linux/remapping-mouse-buttons.html for mappings
   xinput set-button-map "$MOUSE" 1 9 3 4 6 4 7 8
-  SPEED=-0.9
+  SPEED=-0.45
+  xinput --set-prop "$MOUSE" "libinput Scroll Method Enabled" 0 0 1
+  xinput --set-prop "$MOUSE" "libinput Button Scrolling Button" 3
+  xinput --set-prop "$MOUSE" "Evdev Wheel Emulation Axes" 6 7 4 5
 fi
 xinput set-prop "$MOUSE" "libinput Accel Speed" $SPEED
