0ceff05db8cf35d70008c961225365b606189517
tuutti <tuutti@iki.fi>
2020-11-21 13:13:37 +0200

    Fixed mouse script

--- scripts/mouse.sh
+++ scripts/mouse.sh
@@ -5,17 +5,24 @@ if [ $# -eq 0 ]
     echo "No arguments supplied"
     exit 1
 fi
-SPEED=-0.65
+SPEED=-0.39
 MOUSE=$1
+
 
 if [ "$2" == "track" ]; then
   # Remap middleclick to back button
   # @see http://wiki.mbirth.de/know-how/software/linux/remapping-mouse-buttons.html for mappings
   xinput set-button-map "$MOUSE" 1 9 3 4 6 4 7 8
-  xinput --set-prop "$MOUSE" "libinput Scroll Method Enabled" 0 0 1
-  xinput --set-prop "$MOUSE" "libinput Button Scrolling Button" 3
-  xinput --set-prop "$MOUSE" "libinput Accel Profile Enabled" 0 0
-  xinput set-prop "$MOUSE" "libinput Accel Speed" -0.6
-else
-  xinput set-prop "$MOUSE" "libinput Accel Speed" $SPEED
+  SPEED=-0.9
 fi
+
+if [ "$2" == "logitech" ]; then
+  SPEED=0
+fi
+
+for id in $(xinput --list | \
+            sed -n '/'"$MOUSE"'.*pointer/s/.*=\([0-9]\+\).*/\1/p')
+do
+  xinput --set-prop "$id" "libinput Accel Speed" $SPEED
+  xinput --set-prop "$id" 'libinput Accel Profile Enabled' 0, 1
+done
