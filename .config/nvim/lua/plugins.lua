793fe5559283fe9cf5fa80a87857a4fd51a44135
tuutti <tuutti@iki.fi>
2022-02-13 00:04:00 +0200

    More lua config

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,14 +1,17 @@
+vim.cmd [[packadd packer.nvim]]
+
 return require('packer').startup(function(use)
   use {
-    'rose-pine/neovim',
-    as = 'rose-pine',
-    tag = 'v0.*',
-    config = function()
-      vim.cmd('colorscheme rose-pine')
-    end
+    'Raimondi/delimitMate'
   }
+
+  use {
+    'mattn/vim-gist', requires = { 'mattn/webapi-vim' }
+  }
+
   use {
     'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true },
     -- Fix mismatch palette between variants
     event = 'ColorScheme',
     config = function()
@@ -18,6 +21,15 @@ return require('packer').startup(function(use)
           theme = 'rose-pine'
         }
       })
+    end
+  }
+
+  use {
+    'rose-pine/neovim',
+    as = 'rose-pine',
+    tag = 'v0.*',
+    config = function()
+      vim.cmd('colorscheme rose-pine')
     end
   }
 end)
